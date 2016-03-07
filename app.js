099e50fd96f4e44abd7fed959b0a6681e18f2592
Ross Zurowski <ross@rosszurowski.com>
2016-03-06 19:34:10 -0500

    Saving progress

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const koa = require('koa')
 const mount = require('koa-mount')
 const graphql = require('koa-graphql')
 const compress = require('koa-compress')
+const cors = require('koa-cors')
 
 const config = require('./lib/config')
 const schema = require('./lib/schema')
@@ -14,6 +15,7 @@ module.exports = function (opts) {
 	const app = koa()
 	const api = graphql({ schema, pretty: config.api.pretty })
 
+	app.use(cors(config.api.cors))
 	app.use(compress())
 	app.use(mount(config.api.endpoint, api))
 
